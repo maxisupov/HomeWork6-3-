54bdd46746452c9474b9a3d689e9fa14c4b4b8d5
Maxim <max_isupov@mail.ru>
2022-10-10 15:00:33 +0300

    Решил задачу 43

---
+++ 43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+/*if ((x=0) && (x != y))
+{
+    Console.WriteLine($"Прямые параллельны!");
+}*/
+// Попытался допилить, но пишет,
+// что Оператор "&&" невозможно применить к операнду типа "double" и "bool".
+// Моих знаний не хватает))
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами  ({x}, {y})");
